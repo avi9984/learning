5bf8bdb4e9f62adaf965b1060e89cb0fca769f8d
avi9984 <avinashkumar151199@gmail.com>
2026-06-03 17:48:48 +0530

    basic part of js

---
+++ lambda5/basic-js/value_store.js
@@ -0,0 +1,7 @@
+/*What all values we can store in js*/
+
+/*
+1): Number
+2): String
+3): Boolean
+*/
---
+++ lambda5/basic-js/variable_demo.js
@@ -0,0 +1,30 @@
+/*How can create variable in js
+ 3 ways to create variables
+
+ 1. Var keyword 
+ 2. Let keyword
+ 3. Const keyword
+
+ Semicolumn: one complete instruction this called statement.
+ After the completd the code in optinal to use semicolums
+
+
+ Naming rules for variable creation
+ 1): Variable can containe small alphabets, capital alphabet, digits, underscore(_), doller($)
+
+ 2): We can't have space or any other special char.
+
+ 3):We should give meaningfull name so that reader can identify the name properly.
+
+ 4): Variable name cannot start with a digit but it can have digit in beging or at last
+
+ 5): 
+*/
+
+var mark = 90;
+
+let age = 25;
+let flag = 0;
+
+const x = 100;
+const y = 200;
