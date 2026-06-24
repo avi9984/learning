993962f4c65c4c7659bde9fbfdfbd8ae4477e073
avi9984 <avinashkumar151199@gmail.com>
2026-06-24 16:01:05 +0530

    add the some interview questions

---
+++ lambda5/interview_questions.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+
+for (let i = 0; i < 3; i++) {
+    setTimeout(function exec() {
+        console.log("Time done");
+
+    }, 10)
+}
+
+for (let i = 0; i < 10000000000; i++) {
+    // do some task
+}
+
+console.log("end");
+
+
