8b611145869d1d9ef668ad3aaedf181f241697d1
avi9984 <avinashkumar151199@gmail.com>
2026-07-05 19:00:06 +0530

    gbloble in node js

---
+++ lambda5/globlesInNode.js
@@ -0,0 +1,12 @@
+// Globls in NodeJs
+
+//1. __dirname--> It will give you the directy path of the current module
+console.log(__dirname);
+
+//2. require-->It is a function, this can help you to import code from other files
+console.log(require);
+
+//3. process--> It is an object, it gives you information about the current process.
+console.log(process);
+
+
